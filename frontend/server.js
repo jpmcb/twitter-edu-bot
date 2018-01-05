a10d8e84d868b635c1933a0d04bbcda79125531a
John McBride <jpmmcbride@gmail.com>
2018-01-05 15:34:01 -0700

    Initial working version of the front end node app for the twitter bot. Provides a basic POST method to place a users twitter handle into the mongoDB

---
+++ frontend/server.js
@@ -0,0 +1,143 @@
+// John McBride - Jan 05 2017
+// -------
+// Server file for the front end of the educational twitter bot
+
+// Testing environment port
+var port = process.env.PORT || 8080;
+
+// The node mongo client 
+var mongo = require('mongodb').MongoClient;
+
+// The database name
+const dbName = "twitterEdu";
+
+// Local environment port
+var url = "mongodb://localhost:27017/" + dbName;
+
+
+// -----------------------
+// variables and constants for the express 
+// -----------------------
+
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+
+// -----------------------
+// Generates the database for the twitter handles
+// and classes the students are taking
+// -----------------------
+
+var createDB = function() {
+	mongo.connect(url, function(err, client) {
+		if (err) throw err;
+
+		// create the twitter handle collection
+		var db = client.db(dbName);
+		db.createCollection("twitter_handles", function(err, res) {
+			if (err) throw err;
+			console.log("Collection created!");
+			client.close();
+		});
+	});
+}
+
+
+// ---------------------
+// Places a new twitter handle and enrolled class as specified by the user
+// ---------------------
+
+var insertUser = function(first_name, last_name, handle, enrolled) {
+	mongo.connect(url, function(err, client) {
+		if (err) throw err;
+
+		var insert = {
+			handle: handle,
+			first_name: first_name,
+			last_name: last_name,
+			enrolled: enrolled }
+
+		var db = client.db(dbName);
+		db.collection("twitter_handles").insertOne(insert, function(err, res) {
+			if (err) throw err;
+			console.log("User registered in database!");
+			client.close();
+		});
+	});
+}
+
+// -------------------
+// Finds and returns all instances of the enrollments that students are in
+// -------------------
+
+var findClass = function(enrolled, callback) {
+	mngo.connect(url, function(err, client) {
+		var db = client.db(dbName);
+
+		if (err) {
+			console.log(enrolled + " class dose not exist!");
+			client.close();
+		} else {
+			db.collection("twitter_handles").find( {enrolled: enrolled} ), function(err, result) {
+				if (err) throw err;
+				client.close();
+
+				callback(result);
+			}
+		}
+	});
+}
+
+// -----------------
+// Finds and returns the twitter handle for a specific name
+// -----------------
+
+var findHandle = function(first_name, last_name) {
+	mongo.connect(url, function(err, client) {
+		var db = client.db(dbName);
+
+		if (err) {
+			console.log(first_name + " " + last_name + " could not be found!");
+			client.close();
+		} else {
+			db.collection("twitter_handles").findOne( {first_name: first_name, last_name: last_name} ), function(err, result) {
+				if (err) throw err;
+				client.close();
+
+				callback(result);
+			}
+		}
+	})
+}
+
+// --------------
+// --------------
+// Server code for the front end
+// --------------
+// --------------
+
+createDB();
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+app.listen(port, function() {
+	console.log('Listening on port 8080');
+});
+
+app.post('/register', function(req, res) {
+	console.log('User attempting to register ...');
+
+	console.log(req.body);
+	insertUser((req.body.first_name), (req.body.last_name), (req.body.handle), (req.body.enrolled));
+
+	res.render('registered');
+});
